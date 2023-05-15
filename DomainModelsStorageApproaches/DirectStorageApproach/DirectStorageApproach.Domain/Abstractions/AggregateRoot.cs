49cd3c62fa37c7ab6ae6d1023982226721a93db0
Valentin Yuzhakov <yuz-valya@yandex.ru>
2023-05-15 08:18:36 +0300

    Update AggregateRoot.cs

--- DomainModelsStorageApproaches/DirectStorageApproach/DirectStorageApproach.Domain/Abstractions/AggregateRoot.cs
+++ DomainModelsStorageApproaches/DirectStorageApproach/DirectStorageApproach.Domain/Abstractions/AggregateRoot.cs
@@ -7,7 +7,10 @@ public abstract class AggregateRoot : DomainEntity<Guid>
 {
     private readonly List<IDomainEvent> _domainEvents;
 
-    protected AggregateRoot(Guid id) : base(id) { }
+    protected AggregateRoot(Guid id) : base(id) 
+    {
+        _domainEvents = new();
+    }
 
     public IReadOnlyCollection<IDomainEvent> DomainEvents =>
         _domainEvents.AsReadOnly();
